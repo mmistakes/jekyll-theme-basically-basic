771657756f5ab92bbfa139081b04827c1c24799f
Michael Rose <est.michael@gmail.com>
2018-02-16 15:02:53 -0500

    Only include jQuery when using default search
    - Focus on search input after clicking toggle

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -8,7 +8,6 @@
  * https://github.com/mmistakes/jekyll-basically-theme/blob/master/LICENSE.md
 */
 
-// Animate sidebar menu items
 var menuItems = document.querySelectorAll('#sidebar li');
 
 // Get vendor transition property
@@ -16,6 +15,7 @@ var docElemStyle = document.documentElement.style;
 var transitionProp = typeof docElemStyle.transition == 'string' ?
     'transition' : 'WebkitTransition';
 
+// Animate sidebar menu items
 function animateMenuItems() {
   for ( var i=0; i < menuItems.length; i++ ) {
     var item = menuItems[i];
@@ -24,6 +24,13 @@ function animateMenuItems() {
     item.classList.toggle('is--moved');
   }
 };
+
+var myWrapper = document.querySelector('.wrapper');
+var myMenu = document.querySelector('.sidebar');
+var myToggle = document.querySelector('.toggle');
+var myInitialContent = document.querySelector('.initial-content');
+var mySearchContent = document.querySelector('.search-content');
+var mySearchToggle = document.querySelector('.search-toggle');
 
 // Toggle sidebar visibility
 function toggleClassMenu() {
@@ -39,13 +46,11 @@ function toggleClassMenu() {
   }
 }
 
+// Animation smoother
 function OnTransitionEnd() {
   myMenu.classList.remove('is--animatable');
 }
 
-var myWrapper = document.querySelector('.wrapper');
-var myMenu = document.querySelector('.sidebar');
-var myToggle = document.querySelector('.toggle');
 myMenu.addEventListener('transitionend', OnTransitionEnd, false);
 myToggle.addEventListener('click', function() {
   toggleClassMenu();
@@ -55,13 +60,15 @@ myMenu.addEventListener('click', function() {
   toggleClassMenu();
   animateMenuItems();
 }, false);
+mySearchToggle.addEventListener('click', function() {
+  toggleClassSearch();
+}, false);
 
-// Search toggle
-$(".search-toggle").on("click", function() {
-  $(".search-content").toggleClass("is--visible");
-  $(".initial-content").toggleClass("is--hidden");
-  // set focus on input
+// Toggle search input and content visibility
+function toggleClassSearch() {
+  mySearchContent.classList.toggle("is--visible");
+  myInitialContent.classList.toggle("is--hidden");
   setTimeout(function() {
-    $("#search").focus();
+    document.querySelector(".search-content input").focus();
   }, 400);
-});
+}
